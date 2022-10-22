1dc03e55191dac8ed88569ef98894c6ffbb30a19
waleery <walenda1997@gmail.com>
2022-10-22 11:28:13 +0200

    fetching data and picking solution to game

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return (
-    <div className="App">
-    <h1> Wordle</h1>
-    </div>
-  );
+    const [solution, setSolution] = useState(null);
+
+    useEffect(() => {
+        fetch("http://localhost:3001/solutions")
+            .then((res) => res.json())
+            .then((json) => {
+                //random int between 0 and 14
+
+                const randomSolution =
+                    json[Math.floor(Math.random() * json.length)];
+                setSolution(randomSolution.word);
+            });
+    }, [setSolution]);
+
+    console.log(solution);
+
+    return (
+        <div className="App">
+            <h1> Wordle</h1>
+            {solution && <div>Solution is: {solution}</div>}
+        </div>
+    );
 }
 
 export default App;
