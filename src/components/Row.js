4ccbebc2bc9a745d1fdb95c4cf32cb59fa8f8743
waleery <walenda1997@gmail.com>
2022-10-28 14:01:02 +0200

    showing current guess

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,24 +1,40 @@
-import React from 'react'
+import React from "react";
 
-export default function Row({guess}) {
-
-    if(guess){
-      return(
-        <div className="row past">
-          {guess.map((letter, i) => (
-            <div key={i} className={letter.color} >{letter.key}</div>
-          ))}
-        </div>
-      )
+export default function Row({ guess, currentGuess }) {
+    if (guess) {
+        return (
+            <div className="row past">
+                {guess.map((letter, i) => (
+                    <div key={i} className={letter.color}>
+                        {letter.key}
+                    </div>
+                ))}
+            </div>
+        );
     }
 
+    if (currentGuess) {
+        let letters = currentGuess.split(""); //string to letters array
+
+        return (
+            <div className="row current">
+                {letters.map((letter, i) => (
+                    <div key={i} className="filled">{letter}</div>
+                ))}
+                {[...Array(5-letters.length)].map((value, i) =>(
+                    <div key={i}></div>
+                ))}
+            </div>
+        );
+    } 
+
     return (
-    <div className='row'>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-    </div>
-  )
+        <div className="row">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
 }
